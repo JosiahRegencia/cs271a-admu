39bb588d6fe54c6da36e77a8522e91f5270363ed
JosiahRegencia <jawshaeleazar@gmail.com>
2019-06-27 18:28:31 +0800

    Testing branch

---
+++ homework/BFS/tree_traversal.py
@@ -0,0 +1,14 @@
+# Tree traversal using a queue
+
+class Node:
+	def __init__(self, data):
+		self.data  = data
+		self.left  = None
+		self.mid   = None
+		self.right = None
+
+
+class Queue:
+	
+
+
