161aa80c4a2d26a2ae923de0097450be6bee1a5c
christiancqui <45023390+christiancqui@users.noreply.github.com>
2019-06-28 20:22:38 +0800

    Create tree_test.py

---
+++ homework/tree_test.py
@@ -0,0 +1,107 @@
+class Node:
+    def __init__(self,key,level):
+        self.level = level
+        self.key = key
+        self.leftChild = None
+        self.middleChild = None
+        self.rightChild = None
+    # return the key stored in node
+    def getKey(self):
+        return self.key
+
+
+class Tree:
+    def __init__(self):
+        # track number of keys
+        self.keyTracker = 0
+        # track depth of tree
+        self.depth = 0
+        # initialize fringe for BFS
+        self.queue = []
+        #initialize fringe for DFS
+        self.stack = []
+        # root of tree
+        self.root = None
+
+    def expand_bfs(self):
+        # if root
+        if (len(self.queue) == 0):
+            # increment tracking of keys so that root node starts at 1
+            self.keyTracker = self.keyTracker + 1
+            # assign root node to tree object
+            self.root = Node(self.keyTracker, self.depth)
+            # enqueue the root node to queue
+            self.queue.append(self.root)
+        else:
+            # get the 1st in queue, then create new node. points via leftChild
+            self.queue[0].leftChild = Node(self.keyTracker+1,self.queue[0].level+1)
+            # enqueue to fringe
+            self.queue.append(self.queue[0].leftChild)
+            # update max depth if a new level has been reached
+            if (self.queue[0].level > self.depth):
+                self.depth = self.queue[0].level
+            # get the 1st in queue, then create new node. points via middleChild
+            self.queue[0].middleChild = Node(self.keyTracker+2,self.queue[0].level+1)
+            # enqueue to fringe
+            self.queue.append(self.queue[0].middleChild)
+            # update max depth if a new level has been reached
+            if (self.queue[0].level > self.depth):
+                self.depth = self.queue[0].level
+            # get the 1st in queue, then create new node. points via rightChild
+            self.queue[0].rightChild = Node(self.keyTracker+3,self.queue[0].level+1)
+            # enqueue to fringe
+            self.queue.append(self.queue[0].rightChild)
+            # update max depth if a new level has been reached
+            if (self.queue[0].level > self.depth):
+                self.depth = self.queue[0].level
+            # update the tracker (3 because 3 nodes were created)
+            self.keyTracker = self.keyTracker + 3
+            # dequeue the 'incorrect' node
+            self.queue.pop(0)
+
+    # DOESN'T WORK YET
+    def preorder(self,root):
+        if (self.root != None):
+            print(self.root.key)
+            self.preorder(self.root.leftChild)
+            self.preorder(self.root.rightChild)
+
+    # HAVENT TESTED PERO PARANG GOODS
+    def expand_dfs(self):
+        # if root
+        if (len(self.stack) == 0):
+            self.keyTracker = self.keyTracker + 1
+            self.root = Node(self.keyTracker, self.depth)
+            self.stack.append(self.root)
+        else:
+            self.stack[len(stack)-1].leftChild = Node(self.keyTracker+1,self.stack[len(stack)-1].level+1)
+            self.stack.append(self.stack[len(stack)-1].leftChild)
+            if (self.stack[len(stack)-1].level > self.depth):
+                self.depth = self.stack[len(stack)-1].level
+            self.stack[len(stack)-1].middleChild = Node(self.keyTracker+2,self.stack[len(stack)-1].level+1)
+            self.stack.append(self.stack[len(stack)-1].middleChild)
+            if (self.stack[len(stack)-1].level > self.depth):
+                self.depth = self.stack[len(stack)-1].level
+            self.stack[len(stack)-1].rightChild = Node(self.keyTracker+3,self.stack[len(stack)-1].level+1)
+            self.stack.append(self.stack[len(stack)-1].rightChild)
+            if (self.stack[len(stack)-1].level > self.depth):
+                self.depth = self.stack[len(stack)-1].level
+            self.keyTracker = self.keyTracker + 3
+            self.stack.pop()
+
+    # Shows the contents of the queue to track contents and depth level
+    def getQueue(self):
+        for i in self.queue:
+            print("key = {}, depth = {}".format(i.key, i.level))
+
+# driver method FOR MANUAL TESTING
+# test = Tree()
+# test.expand_bfs()
+# test.getQueue()
+# test.preorder(test.root)
+
+# driver method UPDATE THE DESIRED LEVEL INPUT
+level_input = 5
+while (test.depth != level_input):
+    test.expand()
+    test.getQueue()
