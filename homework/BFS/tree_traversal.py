c095c2bd39fc2d8c197d242a4b6e63f7bce91094
JosiahRegencia <jawshaeleazar@gmail.com>
2019-06-28 20:01:40 +0800

    Files

--- homework/BFS/tree_traversal.py
+++ homework/BFS/tree_traversal.py
@@ -1,14 +1,17 @@
 # Tree traversal using a queue
 
+import random
+
+
 class Node:
-	def __init__(self, data):
-		self.data  = data
+	def __init__(self):
 		self.left  = None
 		self.mid   = None
 		self.right = None
 
+	def is_goal():
+		return bool(random.getrandbits(1))
 
-class Queue:
-	
+
 
 
